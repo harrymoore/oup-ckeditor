bee8a959ab81808a11c1909e48badff9aa49d64d
Biren Rao <33708101+birenrao@users.noreply.github.com>
2018-06-13 11:50:55 +0100

    GCMS-1168
    Image alignment options update

--- fields/oup-ckeditor-field.js
+++ fields/oup-ckeditor-field.js
@@ -1,289 +1,237 @@
-define(function (require, exports, module) {
+define(function(require, exports, module) {
+  var $ = require("jquery");
 
-    var $ = require("jquery");
+  var Alpaca = $.alpaca;
 
-    var Alpaca = $.alpaca;
-
-    Alpaca.Fields.OUPCKEditorField = Alpaca.Fields.CKEditorField.extend(
-        /**
-         * @lends Alpaca.Fields.OUPCKEditorField.prototype
-         */
-        {
-            toolbarOptions: {
-                "config1": {
-                    "toolbar": [
-                        [
-                            "Cut",
-                            "Copy",
-                            "Paste",
-                            "-",
-                            "Undo",
-                            "Redo"
-                        ],
-                        [
-                            "Link",
-                            "Unlink",
-                            "Anchor",
-                            "cloudcms-link"
-                        ],
-                        [
-                            "Table",
-                            "HorizontalRule",
-                            "SpecialChar"
-                        ],
-                        [
-                            "Maximize",
-                            "ShowBlocks",
-                            "Source",
-                            "Preview"
-                        ],
-                        [
-                            "Bold",
-                            "Italic",
-                            "Strike",
-                            "Subscript",
-                            "Superscript",
-                            "-",
-                            "RemoveFormat"
-                        ],
-                        [
-                            "NumberedList",
-                            "BulletedList",
-                            "-",
-                            "Outdent",
-                            "Indent",
-                            "Blockquote",
-                            "-",
-                            "JustifyLeft",
-                            "JustifyCenter",
-                            "JustifyRight",
-                            "JustifyBlock"
-                        ],
-                        [
-                            "Format"
-                        ],
-                        [
-                            "cloudcms-image"
-                        ]
-                    ], 
-                    "removeButtons": null,                   
-                    "stylesSet": [
-                        {
-                            "name": "Paragraph",
-                            "element": "p"
-                        },
-                        {
-                            "name": "Heading 2",
-                            "element": "H2"
-                        },
-                        {
-                            "name": "Heading 3",
-                            "element": "H3"
-                        },
-                        {
-                            "name": "Heading 4",
-                            "element": "H4"
-                        }
-                    ],
-                    "cloudcms-image": {
-        "imagePickerType": "file-picker",
-        "imagePickerConfig": {
-            "initialContainerPath": "/",
-            "mimetypeRegex": "image\/([^;\s]+)"
+  Alpaca.Fields.OUPCKEditorField = Alpaca.Fields.CKEditorField.extend(
+    /**
+     * @lends Alpaca.Fields.OUPCKEditorField.prototype
+     */
+    {
+      toolbarOptions: {
+        config1: {
+          toolbar: [
+            ["Cut", "Copy", "Paste", "-", "Undo", "Redo"],
+            ["Link", "Unlink", "Anchor", "cloudcms-link"],
+            ["Table", "HorizontalRule", "SpecialChar"],
+            ["Maximize", "ShowBlocks", "Source", "Preview"],
+            [
+              "Bold",
+              "Italic",
+              "Strike",
+              "Subscript",
+              "Superscript",
+              "-",
+              "RemoveFormat"
+            ],
+            [
+              "NumberedList",
+              "BulletedList",
+              "-",
+              "Outdent",
+              "Indent",
+              "Blockquote",
+              "-",
+              "JustifyLeft",
+              "JustifyCenter",
+              "JustifyRight",
+              "JustifyBlock"
+            ],
+            ["Format"],
+            ["cloudcms-image"]
+          ],
+          removeButtons: null,
+          stylesSet: [
+            {
+              name: "Paragraph",
+              element: "p"
+            },
+            {
+              name: "Heading 2",
+              element: "H2"
+            },
+            {
+              name: "Heading 3",
+              element: "H3"
+            },
+            {
+              name: "Heading 4",
+              element: "H4"
+            }
+          ],
+          "cloudcms-image": {
+            imagePickerType: "file-picker",
+            imagePickerConfig: {
+              initialContainerPath: "/",
+              mimetypeRegex: "image/([^;s]+)"
+            },
+            imageClass: "",
+            enableAltText: false,
+            enableLink: false,
+            linkPickerType: "file-picker",
+            linkPickerConfig: {
+              initialContainerPath: "/"
+            },
+            linkClass: ""
+          },
+          "cloudcms-link": {
+            linkPickerType: "file-picker",
+            linkPickerConfig: {
+              rootContainerPath: "../../..",
+              initialContainerPath: "../"
+            }
+          }
         },
-        "imageClass": "",
-        "enableAltText": false,
-        "enableLink": false,
-        "linkPickerType": "file-picker",
-        "linkPickerConfig": {
-            "initialContainerPath": "/"
+        config2: {
+          toolbar: [["SpecialChar", "Italic", "Subscript", "Superscript"]]
         },
-        "linkClass": ""
-    },
-                    "cloudcms-link": {
-                        "linkPickerType": "file-picker",
-                        "linkPickerConfig": {
-                            "rootContainerPath": "../../..",
-                            "initialContainerPath": "../"
-                        }
-                    },
-                    
-                },
-                "config2": {
-                    "toolbar": [
-                        [
-                            "SpecialChar",
-                            "Italic",
-                            "Subscript",
-                            "Superscript"
-                        ]
-                    ]
-                },
-                "config3": {
-                    "toolbar": [
-                        [
-                            "Cut",
-                            "Copy",
-                            "Paste",
-                            "-",
-                            "Undo",
-                            "Redo"
-                        ],
-                        [
-                            "Link",
-                            "Unlink",
-                            "cloudcms-link"
-                        ],
-                        [
-                            "Bold",
-                            "Italic",
-                            "Strike",
-                            "Subscript",
-                            "Superscript",
-                            "SpecialChar",
-                            "-",
-                            "RemoveFormat"
-                        ],
-                        [
-                            "cloudcms-image"
-                        ]
-                    ],
-                    "removeButtons": null,
-                    "stylesSet": [
-                        {
-                            "name": "Paragraph",
-                            "element": "p"
-                        },
-                        {
-                            "name": "Heading 2",
-                            "element": "H2"
-                        },
-                        {
-                            "name": "Heading 3",
-                            "element": "H3"
-                        },
-                        {
-                            "name": "Heading 4",
-                            "element": "H4"
-                        }
-                    ],
-                    "cloudcms-image": {
-                        "imagePickerType": "file-picker",
-                        "imageUploadPath": "../Image Library",
-                        "imagePickerConfig": {
-                            "rootContainerPath": "../../..",
-                            "initialContainerPath": "../Image Library"
-                        }
-                    },
-                    "cloudcms-link": {
-                        "linkPickerType": "file-picker",
-                        "linkPickerConfig": {
-                            "rootContainerPath": "../../..",
-                            "initialContainerPath": "../Image Library"
-                        }
-                    }
-                },
-                "config4": {
-                    "toolbar": [
-                        [
-                            "Link",
-                            "Unlink",
-                            "cloudcms-link"
-                        ]
-                    ],
-                    "cloudcms-link": {
-                        "linkPickerType": "file-picker",
-                        "linkPickerConfig": {
-                            "initialContainerPath": "/"
-                        }
-                    },
-                    "cloudcms-image": {
-                        "imagePickerType": "file-picker",
-                        "imageUploadPath": "../Image Library",
-                        "imagePickerConfig": {
-                            "rootContainerPath": "../../..",
-                            "initialContainerPath": "../Image Library"
-                        }
-                    },
-                    "cloudcms-link": {
-                        "linkPickerType": "file-picker",
-                        "linkPickerConfig": {
-                            "rootContainerPath": "../../..",
-                            "initialContainerPath": "../Image Library"
-                        }
-                    }
-                },
-                "config5": {
-                    "toolbar": [
-                        [
-                            "Bold",
-                            "Format",
-                            "SpecialChar",
-                            "Italic",
-                            "Subscript",
-                            "Superscript"
-                        ]
-                    ]
-                }
+        config3: {
+          toolbar: [
+            ["Cut", "Copy", "Paste", "-", "Undo", "Redo"],
+            ["Link", "Unlink", "cloudcms-link"],
+            [
+              "Bold",
+              "Italic",
+              "Strike",
+              "Subscript",
+              "Superscript",
+              "SpecialChar",
+              "-",
+              "RemoveFormat"
+            ],
+            ["cloudcms-image"]
+          ],
+          removeButtons: null,
+          stylesSet: [
+            {
+              name: "Paragraph",
+              element: "p"
             },
-
-            /**
-             * @see Alpaca.Fields.TextField#getFieldType
-             */
-            getFieldType: function () {
-                return "oup-ckeditor-field";
+            {
+              name: "Heading 2",
+              element: "H2"
             },
-
-            /**
-             * @see Alpaca.Fields.TextField#setup
-             */
-            setup: function () {
-                if (this.options.ckeditor && this.toolbarOptions[this.options.ckeditor]) {
-                    this.options.ckeditor.format_tags  = 'p;h1;h2;h3;h4;pre';
-                    this.options.ckeditor = this.toolbarOptions[this.options.ckeditor]
-                }
-
-                this.base();
+            {
+              name: "Heading 3",
+              element: "H3"
             },
+            {
+              name: "Heading 4",
+              element: "H4"
+            }
+          ],
+          "cloudcms-image": {
+            imagePickerType: "file-picker",
+            imageUploadPath: "../Image Library",
+            imagePickerConfig: {
+              rootContainerPath: "../../..",
+              initialContainerPath: "../Image Library"
+            }
+          },
+          "cloudcms-link": {
+            linkPickerType: "file-picker",
+            linkPickerConfig: {
+              rootContainerPath: "../../..",
+              initialContainerPath: "../Image Library"
+            }
+          }
+        },
+        config4: {
+          toolbar: [["Link", "Unlink", "cloudcms-link"]],
+          "cloudcms-link": {
+            linkPickerType: "file-picker",
+            linkPickerConfig: {
+              initialContainerPath: "/"
+            }
+          },
+          "cloudcms-image": {
+            imagePickerType: "file-picker",
+            imageUploadPath: "../Image Library",
+            imagePickerConfig: {
+              rootContainerPath: "../../..",
+              initialContainerPath: "../Image Library"
+            }
+          },
+          "cloudcms-link": {
+            linkPickerType: "file-picker",
+            linkPickerConfig: {
+              rootContainerPath: "../../..",
+              initialContainerPath: "../Image Library"
+            }
+          }
+        },
+        config5: {
+          toolbar: [
+            [
+              "Bold",
+              "Format",
+              "SpecialChar",
+              "Italic",
+              "Subscript",
+              "Superscript"
+            ]
+          ]
+        }
+      },
 
-            afterRenderControl: function (model, callback) {
-                var self = this;
-                self.base(model, function () {
-                    callback();
-                });
-            },
+      /**
+       * @see Alpaca.Fields.TextField#getFieldType
+       */
+      getFieldType: function() {
+        return "oup-ckeditor-field";
+      },
 
-            /**
-             * @see Alpaca.Fields.TextField#handleValidate
-             */
-            handleValidate: function () {
-                    return this.base();
-                }
+      /**
+       * @see Alpaca.Fields.TextField#setup
+       */
+      setup: function() {
+        if (
+          this.options.ckeditor &&
+          this.toolbarOptions[this.options.ckeditor]
+        ) {
+          this.options.ckeditor.format_tags = "p;h1;h2;h3;h4;pre";
+          this.options.ckeditor = this.toolbarOptions[this.options.ckeditor];
+        }
 
-                /* builder_helpers */
-                ,
+        this.base();
+      },
 
-            /**
-             * @see Alpaca.Fields.TextField#getTitle
-             */
-            getTitle: function () {
-                return "OUP ckeditor Field";
-            },
-
-            /**
-             * @see Alpaca.Fields.TextField#getDescription
-             */
-            getDescription: function () {
-                return "Allow output markup rule overrides to ckeditor";
-            },
-
-            /* end_builder_helpers */
+      afterRenderControl: function(model, callback) {
+        var self = this;
+        self.base(model, function() {
+          callback();
         });
+      },
 
-    Alpaca.registerMessages({
-        "noDependentField": "No local config found"
-    });
+      /**
+       * @see Alpaca.Fields.TextField#handleValidate
+       */
+      handleValidate: function() {
+        return this.base();
+      },
 
-    Alpaca.registerFieldClass("oup-ckeditor", Alpaca.Fields.OUPCKEditorField);
+      /* builder_helpers */
+      /**
+       * @see Alpaca.Fields.TextField#getTitle
+       */
+      getTitle: function() {
+        return "OUP ckeditor Field";
+      },
 
+      /**
+       * @see Alpaca.Fields.TextField#getDescription
+       */
+      getDescription: function() {
+        return "Allow output markup rule overrides to ckeditor";
+      }
+
+      /* end_builder_helpers */
+    }
+  );
+
+  Alpaca.registerMessages({
+    noDependentField: "No local config found"
+  });
+
+  Alpaca.registerFieldClass("oup-ckeditor", Alpaca.Fields.OUPCKEditorField);
 });
