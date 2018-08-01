21df6a44ed2f0b60a7dd2146399ac6dac9c96798
palak-parikh-oup <palak.parikh@oup.com>
2018-08-01 17:49:02 +0100

    Revert "Update"
    This reverts commit 85b5cee96e6939e8bd8be6e53b91fd451eede1d1.

--- fields/oup-ckeditor-field.js
+++ fields/oup-ckeditor-field.js
@@ -64,8 +64,7 @@ define(function (require, exports, module) {
                         ],
                         [
                             "cloudcms-image",
-                            "cloudcms-iframe",
-                            "ouphello"
+                            "cloudcms-iframe"
                         ]
                     ], 
                     "removeButtons": null,                   
@@ -239,23 +238,6 @@ define(function (require, exports, module) {
                 if (this.options.ckeditor && this.toolbarOptions[this.options.ckeditor]) {
                     this.options.ckeditor.format_tags  = 'p;h1;h2;h3;h4;pre';
                     this.options.ckeditor = this.toolbarOptions[this.options.ckeditor];
-                    this.options.ckeditor.plugins.add(pluginName, {
-                        icons: "ouphello",
-                        init: function (editor) {
-                            // button
-                            editor.ui.addButton(pluginName, {
-                                label: 'Hello',
-                                command: "ouphello"
-                            });
-               
-                            editor.addCommand(pluginName, {
-                                exec: function (editor) {
-                                    editor.insertHtml("<em>Hello</em>");
-                                }
-                            });
-                        }
-                    });
-
                 }
 
                 this.base();
