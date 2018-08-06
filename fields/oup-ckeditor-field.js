d37fd87dfa4be97875f4c0c34de88b4c03090293
ChintanMangukiya-OUP <ChintanMangukiya-OUP@github.com>
2018-08-06 14:54:54 +0530

    GCMS 1114

--- fields/oup-ckeditor-field.js
+++ fields/oup-ckeditor-field.js
@@ -210,7 +210,6 @@ define(function (require, exports, module) {
                         [
                             "H4",
                             "-",
-                            "ouphello",
                             "-",
                             "Italic",
                             "SpecialChar",
