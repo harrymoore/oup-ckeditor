6f091c53ba6fca628a14aa029ab578337260cdca
ChintanMangukiya-OUP <ChintanMangukiya-OUP@github.com>
2018-12-13 19:27:48 +0530

    GCMS-1531

--- fields/oup-ckeditor-field.js
+++ fields/oup-ckeditor-field.js
@@ -411,10 +411,19 @@ define(function (require, exports, module) {
                             "Unlink"
                         ],
                         [
-                            "cloudcms-iframe"
+                            "cloudcms-image"
                         ]
                     ],
-                    "removeButtons": null
+                    "removeButtons": null,
+                    "cloudcms-image": {
+                        "imagePickerType": "file-picker",
+                        "hideUploadButton": true,
+                        "imagePickerConfig": {
+                            "rootContainerPath": "../../..",
+                            "initialContainerPath": "./"
+                        },
+                        "uploadPath": null
+                    }
                 }
             },
 
@@ -435,7 +444,7 @@ define(function (require, exports, module) {
                     this.options.ckeditor = this.toolbarOptions[this.options.ckeditor];
                     if (type && type == "config5") {
                         this.options.ckeditor.format_tags = 'p;h4;pre';
-                    } else if (type && (type == "config1" || type == "config6")) {
+                    } else if (type && (type == "config1" || type == "config6" || type == "config8")) {
                         this.options.ckeditor.format_tags = 'p;h2;h3;h4;pre';
                     } else {
                         this.options.ckeditor.format_tags = 'p;h2;h3;pre';
