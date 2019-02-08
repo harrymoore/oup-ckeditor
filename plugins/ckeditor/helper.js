75939b2c43e06b89bd34bb9fff28432ee29e8811
ChintanMangukiya-OUP <ChintanMangukiya-OUP@github.com>
2019-02-08 07:05:09 +0000

    GCMS-1415

--- plugins/ckeditor/helper.js
+++ plugins/ckeditor/helper.js
@@ -4,10 +4,6 @@ define(function (require, exports, module) {
 
     return {
         registerPlugin: function (pluginId, moduleId) {
-            // var pluginPath = "../../../_modules/wordcount/plugins/ckeditor/wordcount/";
-            // var moduleId = module.uri.match(/^.+(_modules[^\/]+)\/.*/)[1];
-            // '../../../' + moduleId + '/oup-ckeditor-dev/plugins/icons/' + pluginName + '.png'
-            //CKEDITOR.plugins.addExternal(pluginId, moduleId, 'plugin.js');
             var pluginPath = "../../../" + moduleId + "/oup-ckeditor/plugins/ckeditor/wordcount/";
 
             CKEDITOR.plugins.addExternal(pluginId, pluginPath, 'plugin.js');
